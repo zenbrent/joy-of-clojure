f680b784d4fddccf447ccf607c5c4f6a943d9ce5
Brent Brimhall <brent.brimhall@gmail.com>
2016-09-17 20:50:25 -0700

    All the building blocks for A* are done, with a utility fn.

--- chapter-7.4-pathfinding/src/a_star_pathfinding/core.clj
+++ chapter-7.4-pathfinding/src/a_star_pathfinding/core.clj
@@ -1,4 +1,66 @@
 (ns a-star-pathfinding.core)
 
+;; A* Pathfinding!
+
+;; Finds the least-difficult path to a goal.
+;; The difficulty (cost) is found using a function (total-cost) that 
+;; builds an estimate of the total cost from start point to a goal.
+;; This function is used to sort candidate paths in the order most likely
+;; to prove least costly.
+
+(def world [[  1   1   1   1   1]  
+            [999 999 999 999   1]
+            [  1   1   1   1   1]
+            [  1 999 999 999 999]
+            [  1   1   1   1   1]])
+
+;; from 5.1
+(def neighbors4
+  [[-1 0] [1 0] [0 -1] [0 1]])
+
+(def neighbors8
+  (for [l [[-1 0 1]]
+        i l
+        j l
+        :when (not= 0 i j)]
+    [i j]))
+
+
+(defn neighbors
+  "Returns a vector of [x y] pairs for neighboring cells on a grid."
+  ([size yx]
+   (neighbors neighbors4
+              size
+              yx))
+  ([deltas size yx]
+   (filter (fn [new-yx]
+             (every? #(< -1 % size) new-yx))
+           (map #(vec (map + yx %))
+                deltas))))
+
+(defn estimate-cost
+  "This function is often called g."
+  [step-cost-est size y x]
+  (* step-cost-est
+     (- (+ size size) y x 2))) 
+
+(defn path-cost [node-cost cheapest-nbr]
+  "This is often called g, and is implemented as (+ (g ...) (h ...))"
+  (+ node-cost
+     (or (:cost cheapest-nbr) 0))) ;; <-- Add cheapest neighbor cost, else 0.
+
+(defn total-cost [newcost step-cost-est size y x]
+  (+ newcost
+     (estimate-cost step-cost-est size y x)))
+
+;; Good for finding the cheapest path by cost heuristic.
+(defn min-by [f coll]
+  (when (seq coll)
+    (reduce (fn [min other]
+              (if (> (f min) (f other)) ;; Successively bubble the min value out to the return
+                other
+                min))
+            coll)))
+
 
 
--- chapter-7.4-pathfinding/test/a_star_pathfinding/core_test.clj
+++ chapter-7.4-pathfinding/test/a_star_pathfinding/core_test.clj
@@ -2,6 +2,29 @@
   (:require [clojure.test :refer :all]
             [a-star-pathfinding.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest neighbors-test
+  (testing "finding neighbors in a path at a given point"
+    (is (= (neighbors 5 [0 0]) [[1 0] [0 1]]))
+    (is (= (neighbors 5 [2 0]) [[1 0] [3 0] [2 1]]))
+    (is (= (neighbors 5 [2 1]) [[1 1] [3 1] [2 0] [2 2]]))))
+
+(deftest estimate-cost-test
+  (testing "Estimating the cost based on the cost of each step."
+    (is (= (estimate-cost 900 5 0 0) 7200))
+    (is (= (estimate-cost 900 5 4 4) 0))))
+
+(deftest path-cost-test
+  (testing "estimating the cost of the path so far"
+    (is (= (path-cost 900 {:cost 1}) 901))))
+
+(deftest total-cost-test
+  (testing "the total cost of a path"
+    (is (= (total-cost 0 900 5 0 0) 7200))
+    (is (= (total-cost 1000 900 5 3 4) 1900))
+    (is (= (total-cost (path-cost 900 {:cost 1}) 900 5 3 4) 1801))))
+
+(deftest min-by-test
+  (testing "finding the min by a function"
+    (is (= {:cost 1} (min-by :cost [{:cost 100} {:cost 110} {:cost 1} {:cost 99} {:cost 1000}])))
+    (is (= {:cost -10} (min-by :cost [{:cost 10} {:cost -10}])))))
+
